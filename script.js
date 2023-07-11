e18ce205a732cfe786261cf186922d970c146a97
jenstem <124745888+jenstem@users.noreply.github.com>
2023-07-11 15:14:30 -0400

    Format js

--- script.js
+++ script.js
@@ -15,92 +15,91 @@ saveTheBtns();
 
 searchedCityEL.style.display = 'none';
 
-function setTime() {
+function setTime(){
     let today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     $("#currentTime").text(today);
 
-    function updateTime() {
-        today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-        $("#currentTime").text(today);
+    function updateTime(){
+      today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+      $("#currentTime").text(today);
     }
     setInterval(updateTime, 1000);
-}
-setTime();
-
+  }
+  setTime();
 
 searchBtn.addEventListener("click", function () {
-    var locateCity = searchText.value.trim();
-    if (locateCity == '') return;
-    whatsTheWeather(locateCity);
-    saveTheCityBtn(locateCity);
-    saveCity(locateCity);
-    searchText.value = "";
-    searchEl.classList.remove('col-12')
-    searchEl.classList.add('col-4')
-    searchedCityEL.style.display = '';
+  var locateCity = searchText.value.trim();
+  if (locateCity == '') return;
+  whatsTheWeather(locateCity);
+  saveTheCityBtn(locateCity);
+  saveCity(locateCity);
+  searchText.value = "";
+  searchEl.classList.remove('col-12')
+  searchEl.classList.add('col-4')
+  searchedCityEL.style.display = '';
+});
+
+function saveTheCityBtn(locateCity) {
+  var searchingButton = document.createElement("button");
+  searchingButton.classList.add("btn", "btn-outline-secondary", "w-100");
+  searchingButton.textContent = locateCity;
+  searchedCities.appendChild(searchingButton);
+  searchingButton.addEventListener("click", function (event) {
+    var citySearch = event.target.textContent;
+    saveCityList(citySearch);
   });
+}
 
-  function saveTheCityBtn(locateCity) {
-    var searchingButton = document.createElement("button");
-    searchingButton.classList.add("btn", "btn-outline-secondary", "w-100");
-    searchingButton.textContent = locateCity;
-    searchedCities.appendChild(searchingButton);
-    searchingButton.addEventListener("click", function (event) {
-      var citySearch = event.target.textContent;
-      saveCityList(citySearch);
-    });
+function saveCityList(citySearch) {
+  whatsTheWeather(citySearch)
+  searchEl.classList.remove('col-12')
+  searchEl.classList.add('col-4')
+  searchedCityEL.style.display = '';
+}
+
+
+function saveCity(citySearch) {
+  var savedCities = localStorage.getItem("savedCities");
+  if (savedCities) {
+    savedCities = JSON.parse(savedCities);
+  } else {
+    savedCities = [];
   }
+  savedCities.push(citySearch);
+  localStorage.setItem("savedCities", JSON.stringify(savedCities));
+}
 
-  function saveCityList(citySearch) {
-    whatsTheWeather(citySearch)
-    searchEl.classList.remove('col-12')
-    searchEl.classList.add('col-4')
-    searchedCityEL.style.display = '';
-  }
-
-
-  function saveCity(citySearch) {
-    var savedCities = localStorage.getItem("savedCities");
-    if (savedCities) {
-      savedCities = JSON.parse(savedCities);
-    } else {
-      savedCities = [];
-    }
-    savedCities.push(citySearch);
-    localStorage.setItem("savedCities", JSON.stringify(savedCities));
-  }
-
-  function saveTheBtns() {
-    var savedCities = localStorage.getItem("savedCities");
-    if (savedCities) {
-      savedCities = JSON.parse(savedCities);
-      savedCities.innerHTML = "";
-      for (let i = 0; i < savedCities.length; i++) {
-          saveTheCityBtn(savedCities[i]);
-      }
+function saveTheBtns() {
+  var savedCities = localStorage.getItem("savedCities");
+  if (savedCities) {
+    savedCities = JSON.parse(savedCities);
+    savedCities.innerHTML = "";
+    for (let i = 0; i < savedCities.length; i++) {
+        saveTheCityBtn(savedCities[i]);
     }
   }
+}
 
 function whatsTheWeather(city) {
-    var apiKey = "26d18b24e744af5b39443da096b25939";
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=" + apiKey;
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            getWeather(response);
-        });
+  var apiKey = "26d18b24e744af5b39443da096b25939";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=" + apiKey;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      getWeather(response);
+    });
 
-    function getWeather(response) {
-        var cityName = response.city.name;
-        var getTemp = response.list[0].main.temp;
-        var getWind = response.list[0].wind.speed;
-        var getHumid = response.list[0].main.humidity;
-        city.textContent = cityName;
-        tempEL.textContent = getTemp + "°F";
-        windEl.textContent = getWind + " mph";
-        humidEl.textContent = getHumid + "%";
+  function getWeather(response) {
+    var cityName = response.city.name;
+    var getTemp = response.list[0].main.temp;
+    var getWind = response.list[0].wind.speed;
+    var getHumid = response.list[0].main.humidity;
+    city.textContent = cityName;
+    tempEL.textContent = getTemp + "°F";
+    windEl.textContent = getWind + " mph";
+    humidEl.textContent = getHumid + "%";
 
     // initialize with empty string
     var weatherCard = "";
@@ -113,7 +112,7 @@ function whatsTheWeather(city) {
       // concat a string of HTML elements to the weatherCard variable
       // use template literals to create dynamic values within the string
       // add a new multiple weather forecast, create multiple cards or days
-      // icons added in line 144
+      // icons line 144
       weatherCard += `<div class="col-3 col-lg-2 col-md-4 col-sm-3">
             <h5>${getDate}</h5>
             <img src="http://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png"/>
